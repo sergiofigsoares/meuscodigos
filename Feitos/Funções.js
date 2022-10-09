29c96a24698568a8b55e5428f220ca74c7c0b338
RaulCostaSoares <raulcostasoares2005@gmail.com>
2022-10-09 02:03:56 -0300

    PingPong Feito

--- Feitos/Funções.js
+++ Feitos/Funções.js
@@ -1,17 +1,17 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function desenha2d(cor, xi, yi, Xf, Yf){
+function desenha2d(cor, xi, yi, Xf, Yf) {
 
     var tela = document.querySelector("canvas");
     var pincel = tela.getContext("2d");
 
     pincel.fillStyle = cor;
     pincel.fillRect(xi, yi, Xf, Yf);
-    
+
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-            
-function desenhaQuadrado(x, y, cor){
+
+function desenhaQuadrado(x, y, cor) {
 
     var tela = document.querySelector('canvas');
     var pincel = tela.getContext('2d');
@@ -21,6 +21,21 @@ function desenhaQuadrado(x, y, cor){
     pincel.fillStroke = 'black'
     pincel.fillRect(x, y, 50, 50)
 
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+document.onkeydown = (e) => {
+    e = e || window.event;
+    if (e.keyCode === 38) {
+        console.log('up arrow pressed')
+    } else if (e.keyCode === 40) {
+        console.log('down arrow pressed')
+    } else if (e.keyCode === 37) {
+        console.log('left arrow pressed')
+    } else if (e.keyCode === 39) {
+        console.log('right arrow pressed')
+    }
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
